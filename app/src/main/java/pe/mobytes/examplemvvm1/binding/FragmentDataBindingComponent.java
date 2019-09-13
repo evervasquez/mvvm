cc5eccee301caee5a81fef40439d4e4acb4032b5
ever vasquez <ever@mobytes.pe>
2019-09-13 17:49:35 -0500

    add FragmentDataBindingComponent

---
+++ app/src/main/java/pe/mobytes/examplemvvm1/binding/FragmentDataBindingComponent.java
@@ -0,0 +1,15 @@
+package pe.mobytes.examplemvvm1.binding;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentDataBindingComponent implements androidx.databinding.DataBindingComponent {
+    private final FragmentBindingAdapters adapters;
+
+    public FragmentDataBindingComponent(Fragment fragment) {
+        this.adapters = new FragmentBindingAdapters(fragment);
+    }
+
+    public FragmentBindingAdapters getFragmentBindingAdapters() {
+        return adapters;
+    }
+}
