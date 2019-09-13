6477588357fe609abf5a987a4a30edeff5f9c641
ever vasquez <ever@mobytes.pe>
2019-09-13 18:18:19 -0500

    add DataBoundListAdapter

---
+++ app/src/main/java/pe/mobytes/examplemvvm1/ui/common/DataBoundViewHolder.java
@@ -0,0 +1,16 @@
+package pe.mobytes.examplemvvm1.ui.common;
+
+import androidx.databinding.ViewDataBinding;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DataBoundViewHolder<T extends ViewDataBinding> extends RecyclerView.ViewHolder {
+
+    public final  T binding;
+
+    DataBoundViewHolder(T binding){
+        super(binding.getRoot());
+        this.binding = binding;
+    }
+
+
+}
